fb37dad66746b9fd5e7b2aa89ca62df1f4174b71
Kolitha Perera <kolitha.perera@scania.com>
2025-10-30 20:04:03 +0100

    Added the exception to the lambada function to throw exceptions randomly.

--- functions/fetch_order/fetch_order.py
+++ functions/fetch_order/fetch_order.py
@@ -13,9 +13,9 @@ def handler(event, context):
     logger.info(f"Executing fetch_order with event: {json.dumps(event)}")
 
     # Generate random number between 1 and 4 and throw exception if odd
-    random_number = random.randint(1, 4)
+    random_number = random.randint(1, 5)
     logger.info(f"Generated random number: {random_number}")
-    if random_number % 2 != 0:
+    if random_number % 2 == 0:
         raise Exception(f"Random number {random_number} is odd, throwing exception")
 
     # Create and return order as JSON with required fields
